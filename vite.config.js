7e89aa0534c412d275804f6aeb6995f92132d0bf
Ali-Fawzi-Lateef <alialdarraji095@gmail.com>
2023-05-04 21:29:05 +0300

    Merge remote-tracking branch 'origin/main' into main

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/managerDashboardCharts.js',
+                'resources/js/delete&edit_manager.js'
             ],
             refresh: true,
         }),
